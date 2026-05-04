39e5202798d3280c66c0d5b5f9a26b7342ab65bc
atalasluis <atalasturey@gmail.com>
2026-05-04 01:47:54 -0400

    info

--- Esp32_Smart_Object/src/AWSManager.h
+++ Esp32_Smart_Object/src/AWSManager.h
@@ -26,12 +26,61 @@ public:
 };
 
 /*
+Device Shadow state
 {
   "state": {
+    "desired": {
+      "led": "off"
+    },
     "reported": {
-      "distancia": 25,
-      "led": "green"
+      "distancia": 22,
+      "led": "off"
     }
   }
 }
+Device Shadow metadata
+{
+  "metadata": {
+    "desired": {
+      "led": {
+        "timestamp": 1777871239
+      }
+    },
+    "reported": {
+      "distancia": {
+        "timestamp": 1777871364
+      },
+      "led": {
+        "timestamp": 1777871364
+      }
+    }
+  }
+}
+
+topic para publicar en mqtt test client
+
+Topic name
+$aws/things/thing_test/shadow/update
+
+Message payload
+{
+  "state": {
+    "desired": {
+      "led": "off"
+    }
+  }
+}
+
+topic para suscribirse y ver el intercambio de mensajes
+
+$aws/things/thing_test/shadow/#
+
+las claves para que el esp32 se conecte a IoT core estan en:
+
+ESP32_SMART_OBJECT/include/certs/
+
+device_cert.h
+private_key.h
+root_ca.h
+
 */
